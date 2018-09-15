93fc622d4f3604a89f179a5e18972cb442313170
野村 修 <osamu_nomura@outlook.jp>
2018-09-15 17:00:57 +0900

    IOFilterの追加

--- wpf/Utilities/IOFilters.cs
+++ wpf/Utilities/IOFilters.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using hsb.Utilities;
 
 
 namespace hsb.WPF.Utilities
@@ -23,9 +24,7 @@ namespace hsb.WPF.Utilities
         /// <param name="direction">入出力方向</param>
         /// <returns>出力文字列</returns>
         public static string ToUpper(string s, IOFilterDirection direction)
-        {
-            return direction.IsOutput() ? s : s.ToUpper();
-        }
+            => direction.IsOutput() ? s : s.ToUpper();
         #endregion
 
         #region - ToLower : 小文字変換
@@ -36,9 +35,7 @@ namespace hsb.WPF.Utilities
         /// <param name="direction">入出力方向</param>
         /// <returns>出力文字列</returns>
         public static string ToLower(string s, IOFilterDirection direction)
-        {
-            return direction.IsOutput() ? s : s.ToLower();
-        }
+            => direction.IsOutput() ? s : s.ToLower();
         #endregion
 
         #region - Trim : 先頭・末尾の空白文字を除去する
@@ -49,9 +46,7 @@ namespace hsb.WPF.Utilities
         /// <param name="direction">入出力方向</param>
         /// <returns>出力文字列</returns>
         public static string Trim(string s, IOFilterDirection direction)
-        {
-            return direction.IsOutput() ? s : s.Trim();
-        }
+            => direction.IsOutput() ? s : s.Trim();
         #endregion
 
         #region - TrimStart : 先頭の空白文字を除去する
@@ -62,9 +57,7 @@ namespace hsb.WPF.Utilities
         /// <param name="direction">入出力方向</param>
         /// <returns>出力文字列</returns>
         public static string TrimStart(string s, IOFilterDirection direction)
-        {
-            return direction.IsOutput() ? s : s.TrimStart();
-        }
+            => direction.IsOutput() ? s : s.TrimStart();
         #endregion
 
         #region - TrimEnd : 末尾の空白文字を除去する
@@ -75,9 +68,29 @@ namespace hsb.WPF.Utilities
         /// <param name="direction">入出力方向</param>
         /// <returns>出力文字列</returns>
         public static string TrimEnd(string s, IOFilterDirection direction)
-        {
-            return direction.IsOutput() ? s : s.TrimEnd();
-        }
+            => direction.IsOutput() ? s : s.TrimEnd();
+        #endregion
+
+        #region - Han2Zen : 半角文字を全角にする
+        /// <summary>
+        /// 半角文字を全角にする
+        /// </summary>
+        /// <param name="s">入力文字列</param>
+        /// <param name="direction">入出力方向</param>
+        /// <returns>出力文字列</returns>
+        public static string Han2Zen(string s, IOFilterDirection direction)
+            => direction.IsOutput() ? s : MapString.Han2Zen(s);
+        #endregion
+
+        #region - Zen2Han : 全角文字を半角にする
+        /// <summary>
+        /// 全角文字を半角にする
+        /// </summary>
+        /// <param name="s">入力文字列</param>
+        /// <param name="direction">入出力方向</param>
+        /// <returns>出力文字列</returns>
+        public static string Zen2Han(string s, IOFilterDirection direction)
+            => direction.IsOutput() ? s : MapString.Zen2Han(s);
         #endregion
 
         #endregion
